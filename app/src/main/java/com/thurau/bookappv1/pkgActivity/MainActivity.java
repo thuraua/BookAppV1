dedc3aec9d4ef2211688ae5dc82db8983d23aff8
Alexander THURAU <thuraua@edu.htl-villach.at>
2019-05-24 09:04:18 +0200

    Initial commit

---
+++ app/src/main/java/com/thurau/bookappv1/pkgActivity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.thurau.bookappv1.pkgActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.thurau.bookappv1.R;
+import com.thurau.bookappv1.pkgData.Book;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtAddress;
+    private EditText txtId;
+    private EditText txtTitle;
+    private EditText txtAuthor;
+    private Spinner spBooks;
+    private ArrayAdapter<Book> adapterBooks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getAllViews();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        try {
+            switch (id) {
+                case R.id.mitem_get:
+
+                    break;
+                case R.id.mitem_list:
+
+                    break;
+                case R.id.mitem_insert:
+
+                    break;
+                case R.id.mitem_update:
+
+                    break;
+                case R.id.mitem_delete:
+
+                    break;
+
+            }
+        } catch (Exception ex) {
+            Toast.makeText(this, "Exception in main activity: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getAllViews() {
+        txtAddress = findViewById(R.id.txtAddress);
+        txtId = findViewById(R.id.txtId);
+        txtTitle = findViewById(R.id.txtTitle);
+        txtAuthor = findViewById(R.id.txtAuthor);
+        spBooks = findViewById(R.id.spBooks);
+        adapterBooks = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+    }
+}
---
+++ app/src/main/java/com/thurau/bookappv1/pkgData/Book.java
@@ -0,0 +1,49 @@
+package com.thurau.bookappv1.pkgData;
+
+public class Book {
+    private int id;
+    private String title = null,
+            author = null;
+
+    public Book() {
+        this(-99, "no book", "no authors");
+    }
+
+    public Book(int id, String title, String author) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", author=" + author + '}';
+    }
+
+
+}
